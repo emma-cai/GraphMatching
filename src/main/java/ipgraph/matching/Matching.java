55edef7bf844e4923cb0ee0971a679ca4bbb90c4
emma-cai <qingqing.cai@temple.edu>
2015-05-05 22:11:50 -0400

    convert graph to similarity_flooding_input; run similarity flooding algorithm;

--- src/main/java/ipgraph/matching/Matching.java
+++ src/main/java/ipgraph/matching/Matching.java
@@ -1,9 +1,16 @@
 package ipgraph.matching;
 
+import com.interdataworking.mm.alg.MapPair;
+import com.interdataworking.mm.alg.Match;
 import ipgraph.datastructure.DGraph;
 import ipgraph.datastructure.DNode;
+import org.jgrapht.graph.DefaultEdge;
+import org.w3c.rdf.model.*;
+import org.w3c.rdf.util.RDFFactory;
+import org.w3c.rdf.util.RDFFactoryImpl;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by qingqingcai on 5/4/15.
@@ -13,49 +20,85 @@ public class Matching {
     private static boolean debug = true;
 
     /** **************************************************************
-     * Compute similarity cost between two graphs. Higher cost indicates
-     * lower similarity.
+     * Create Similarity Flooding input model from DGraph
      */
-    public static double GraphSimilarityCost(DGraph TGraph, DGraph HGraph) {
+    static Model createModelFromGraph(DGraph graph, ArrayList<Resource> NodeResources) throws Exception {
 
-        double COST = 0.0;
+        RDFFactory rf = new RDFFactoryImpl();
+        NodeFactory nf = rf.getNodeFactory();
 
-        int TMaxLevel = TGraph.addNodeLevel();
-        int HMaxLevel = HGraph.addNodeLevel();
-        int MaxLevel = Math.min(TMaxLevel, HMaxLevel);
+        // create graph/model M
+        Model M = rf.createModel();
 
-        for (int level = 1; level <= MaxLevel; level++) {
-            Set<DNode> TDNodesInLevel = TGraph.getNodesByLevel(level);
-            Set<DNode> HDNodesInLevel = HGraph.getNodesByLevel(level);
-            for (DNode TDNode : TDNodesInLevel) {
-                for (DNode HDNode : HDNodesInLevel) {
-                    if (debug) System.out.println(TDNode.getForm() + "\t" + HDNode.getForm() + "\t");
-                    double cost = 0.0;
-                    if (TDNode.getDepLabel().equals(HDNode.getDepLabel())) {
-                        cost = DNodeSimilarity(TDNode, HDNode);
-                        COST += cost;
-                        if (debug) System.out.println("\t" + cost + "\t" + COST);
-                        break;
-                    } else {
-                        cost = 0.3;
-                        COST += cost;
-                    }
+        for (DefaultEdge edge : graph.edgeSet()) {
+            DNode source = graph.getEdgeSource(edge);
+            DNode target = graph.getEdgeTarget(edge);
+            Resource s = nf.createResource(source.getForm());
+            Resource t = nf.createResource(target.getForm());
+            Resource e = nf.createResource(target.getDepLabel());
 
-                    if (debug) System.out.println("\t" + cost + "\t" + COST);
-                }
-            }
+            NodeResources.add(s);
+            NodeResources.add(t);
+
+            M.add(nf.createStatement(s, e, t));
         }
 
-        return COST;
+        return M;
     }
 
     /** **************************************************************
-     * Compute similarity cost between two nodes;
+     * Compute the similarity between two DGraphs' nodes
      */
-    public static double DNodeSimilarity(DNode n1, DNode n2) {
+    static void computeNodeSimilarity(DGraph dgraph1, DGraph dgraph2) throws Exception {
 
-        if (n1.getLemma().equals(n2.getLemma()))
-            return 0.0;
-        return 1.0;
+        ArrayList<Resource> NodeResourcesDGraph1 = new ArrayList<>();
+        ArrayList<Resource> NodeResourcesDGraph2 = new ArrayList<>();
+
+        Model A = createModelFromGraph(dgraph1, NodeResourcesDGraph1);
+        Model B = createModelFromGraph(dgraph2, NodeResourcesDGraph2);
+
+        // create an initial mapping which is just a cross-product with 1's as weights
+        List initMap = new ArrayList();
+        for (Resource r1 : NodeResourcesDGraph1) {
+            for (Resource r2 : NodeResourcesDGraph2) {
+                initMap.add(new MapPair(r1, r2, 1.0));
+            }
+        }
+
+        Match sf = new Match();
+
+        // Two lines below are used to get the same setting as in the example of the ICDE'02 paper.
+        // (In general, this formula won't converge! So better stick to the default values instead)
+        sf.formula = com.interdataworking.mm.alg.Match.FORMULA_FFT;
+        sf.FLOW_GRAPH_TYPE = com.interdataworking.mm.alg.Match.FG_PRODUCT;
+
+        MapPair[] result = sf.getMatch(A, B, initMap);
+        MapPair.sort(result);
+
+        // Answer Extraction
+        String answer = extractAnswer(dgraph1, result);
+        System.out.println("Answer = " + answer);
+    }
+
+    /** **************************************************************
+     * Extract answers, defined as the node with highest similarity
+     * score with WH-node
+     */
+    static String extractAnswer(DGraph graph, MapPair[] result) throws ModelException {
+
+        String answer = "";
+        for (MapPair mp : result) {
+            RDFNode leftNode = mp.getLeftNode();
+            RDFNode rightNode = mp.getRightNode();
+            if (rightNode.getLabel().equals("What")) {
+                System.out.println();
+                System.out.println("mp.left = " + leftNode.getLabel());
+                System.out.println("mp.right = " + rightNode.getLabel());
+                System.out.println("mp.similarity = " + mp.sim);
+                answer = leftNode.getLabel();
+                return answer;
+            }
+        }
+        return answer;
     }
 }
