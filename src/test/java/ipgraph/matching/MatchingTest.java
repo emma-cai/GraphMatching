b8163c44f2afce01cd52c3c809e75097e61ea3c3
geraldkurlandski <gerald.kurlandski@ipsoft.com>
2015-05-12 12:03:04 -0400

    New test.

--- src/test/java/ipgraph/matching/MatchingTest.java
+++ src/test/java/ipgraph/matching/MatchingTest.java
@@ -1,5 +1,6 @@
 package ipgraph.matching;
 
+import com.interdataworking.mm.alg.DSimplifiedMatch;
 import com.interdataworking.mm.alg.SimplifiedMatch;
 import com.interdataworking.mm.alg.MapPair;
 import com.interdataworking.mm.alg.Match;
@@ -64,10 +65,10 @@ public class MatchingTest {
     }
 
     @Test
-    public void testGoodMatch_UsingCompareGraphs() throws ModelException {
-        SimplifiedMatch compareGraphs = new SimplifiedMatch();
+    public void testGoodMatch_UsingSimplifiedMatch() throws ModelException {
+        SimplifiedMatch match = new SimplifiedMatch();
 
-        Matching matching = new Matching(compareGraphs);
+        Matching matching = new Matching(match);
 
         String s1 = "Some proteins, such as those to be incorporated in membranes (known as membrane proteins), are transported into the RER during synthesis.";
         DTree dtree1 = DTree.buildTree(s1);
@@ -86,10 +87,54 @@ public class MatchingTest {
     }
 
     @Test
-    public void testBadMatch_UsingCompareGraphs() throws ModelException {
-        SimplifiedMatch compareGraphs = new SimplifiedMatch();
+    public void testBadMatch_UsingSimplifiedMatch() throws ModelException {
+        SimplifiedMatch match = new SimplifiedMatch();
 
-        Matching matching = new Matching(compareGraphs);
+        Matching matching = new Matching(match);
+
+        String s1 = "Biosynthesis (also called biogenesis) is an enzyme-catalyzed process in cells of living organisms by which substrates are converted to more complex products (also simply known as protein translation).";
+        DTree dtree1 = DTree.buildTree(s1);
+        DGraph dgraph1 = DGraph.buildDGraph(dtree1).getSubgraph(Matching.postagSet);
+
+        String s2 = "What are transported into the RER during synthesis?";
+        DTree dtree2 = DTree.buildTree(s2);
+        DGraph dgraph2 = DGraph.buildDGraph(dtree2).getSubgraph(Matching.postagSet);
+
+        MapPair[] actualPairs =  matching.computeNodeSimilarity(dgraph1, dgraph2);
+        MapPair actual = Matching.extractAnswer(actualPairs, "What");
+
+        assertEquals("substrates", actual.getLeftNode().getLabel());
+        assertEquals("What", actual.getRightNode().getLabel());
+        assertEquals(0.515, actual.sim, .01);
+    }
+
+    @Test
+    public void testGoodMatch_UsingDSimplifiedMatch() throws ModelException {
+        DSimplifiedMatch match = new DSimplifiedMatch();
+
+        Matching matching = new Matching(match);
+
+        String s1 = "Some proteins, such as those to be incorporated in membranes (known as membrane proteins), are transported into the RER during synthesis.";
+        DTree dtree1 = DTree.buildTree(s1);
+        DGraph dgraph1 = DGraph.buildDGraph(dtree1).getSubgraph(Matching.postagSet);
+
+        String s2 = "What are transported into the RER during synthesis?";
+        DTree dtree2 = DTree.buildTree(s2);
+        DGraph dgraph2 = DGraph.buildDGraph(dtree2).getSubgraph(Matching.postagSet);
+
+        MapPair[] actualPairs =  matching.computeNodeSimilarity(dgraph1, dgraph2);
+        MapPair actual = Matching.extractAnswer(actualPairs, "What");
+
+        assertEquals("proteins", actual.getLeftNode().getLabel());
+        assertEquals("What", actual.getRightNode().getLabel());
+        assertEquals(0.329, actual.sim, .01);
+    }
+
+    @Test
+    public void testBadMatch_UsingDSimplifiedMatch() throws ModelException {
+        DSimplifiedMatch match = new DSimplifiedMatch();
+
+        Matching matching = new Matching(match);
 
         String s1 = "Biosynthesis (also called biogenesis) is an enzyme-catalyzed process in cells of living organisms by which substrates are converted to more complex products (also simply known as protein translation).";
         DTree dtree1 = DTree.buildTree(s1);
