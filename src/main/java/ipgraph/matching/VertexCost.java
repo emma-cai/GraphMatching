b9cbad3c87f8f8dbf919278c50c4963e1447ed11
emma-cai <qingqing.cai@temple.edu>
2015-05-14 16:28:03 -0400

    add function to compute WordNet similarity using ws4j

--- src/main/java/ipgraph/matching/VertexCost.java
+++ src/main/java/ipgraph/matching/VertexCost.java
@@ -1,34 +1,50 @@
 package ipgraph.matching;
 
+import edu.cmu.lti.lexical_db.ILexicalDatabase;
+import edu.cmu.lti.lexical_db.NictWordNet;
+import edu.cmu.lti.ws4j.RelatednessCalculator;
+import edu.cmu.lti.ws4j.impl.Path;
+import edu.cmu.lti.ws4j.util.WS4JConfiguration;
 import ipgraph.datastructure.DNode;
+import org.apache.commons.math.stat.StatUtils;
+
+import java.util.Collections;
+import java.util.Vector;
 
 /**
  * Created by qingqingcai on 5/14/15.
  */
 public class VertexCost implements NodeComparer {
 
-    private static boolean debug = false;
+    private static boolean debug = true;
 
-    private static int vertexFeaNum = 3;
+    private static ILexicalDatabase db = new NictWordNet();
 
-    /** **************************************************************
-     * Implement  method getNodeSimilarity in NodeComparer: Calculcate
-     * a value from 0 to 1 inclusive that indicates how similar the
-     * two nodes are.
-     */
-    @Override
-    public double getNodeSimilarity(DNode dnode_T, DNode dnode_H) {
+    private static RelatednessCalculator[] rcs = {
+            /**new HirstStOnge(db), new LeacockChodorow(db), new Lesk(db),  new WuPalmer(db),
+            new Resnik(db), new JiangConrath(db), new Lin(db), **/new Path(db)
+    };
 
-        double[] fie_vector = new double[vertexFeaNum];
-        double exactMatch_tag = isExactMatch(dnode_T, dnode_H);
-        double lemmaMatch_tag = isLemmaMatch(dnode_T, dnode_H);
-        double posMatch_tag = isPOSMatch(dnode_T, dnode_H);
+    public double getNodeCost(DNode dnode_T, DNode dnode_H) {
 
-        double[] fie_weight_vector = {0.4, 0.4, 0.2};
+        Vector<Double> fie_vector = new Vector<>();
+        double exactMatch_tag = doExactMatch(dnode_T, dnode_H);
+        double lemmaMatch_tag = doLemmaMatch(dnode_T, dnode_H);
+        double posMatch_tag = doPOSMatch(dnode_T, dnode_H);
+        double ws4jMatch_tag = doWS4JMatch(dnode_T, dnode_H);
 
-        fie_vector[0] = exactMatch_tag;
-        fie_vector[1] = lemmaMatch_tag;
-        fie_vector[2] = posMatch_tag;
+        fie_vector.add(exactMatch_tag);
+        fie_vector.add(lemmaMatch_tag);
+        fie_vector.add(posMatch_tag);
+        fie_vector.add(ws4jMatch_tag);
+
+        Double[] weights = {0.3, 0.2, 0.2, 0.3};
+        Vector<Double> fie_weight_vector = new Vector();
+        Collections.addAll(fie_weight_vector, weights);
+//        Vector<Double> fie_weight_vector = new Vector<>(fie_vector.size());
+//        for (int i = 0; i < fie_vector.size(); i++)
+//            fie_weight_vector.add(1.0/fie_vector.size());
+
 
         double VertexSub = computeExpFun(fie_weight_vector, fie_vector);
 
@@ -50,10 +66,23 @@ public class VertexCost implements NodeComparer {
     }
 
     /** **************************************************************
+     * Implement  method getNodeSimilarity in NodeComparer: Calculcate
+     * a value from 0 to 1 inclusive that indicates how similar the
+     * two nodes are.
+     */
+    @Override
+    public double getNodeSimilarity(DNode dnode_T, DNode dnode_H) {
+
+        double nodeMatchingCost = getNodeSimilarity(dnode_T, dnode_H);
+        double nodeSimilarity = 1 - nodeMatchingCost;
+        return nodeSimilarity;
+    }
+
+    /** **************************************************************
      * If v and M(v) are identical words; return 0 (cost value) if they
      * are, otherwise return 1;
      */
-    private static double isExactMatch(DNode dnode_T, DNode dnode_H) {
+    private static double doExactMatch(DNode dnode_T, DNode dnode_H) {
         if (dnode_T.getForm().equals(dnode_H.getForm()))
             return 0.0;             // if exactly matched, the cost is 0
         else
@@ -64,7 +93,7 @@ public class VertexCost implements NodeComparer {
      * If v and M(v) have the same lemma; return 0 (cost value) if they
      * match, otherwise return 1;
      */
-    private static double isLemmaMatch(DNode dnode_T, DNode dnode_H) {
+    private static double doLemmaMatch(DNode dnode_T, DNode dnode_H) {
         if (dnode_T.getLemma().equals(dnode_H.getLemma()))
             return 0.0;
         else
@@ -75,9 +104,21 @@ public class VertexCost implements NodeComparer {
      * If v and M(v) have the same part of speech; return 0 (cost value)
      * if they match, otherwise return 1;
      */
-    private static double isPOSMatch(DNode dnode_T, DNode dnode_H) {
+    private static double doPOSMatch(DNode dnode_T, DNode dnode_H) {
         if (dnode_T.getPOS().equals(dnode_H.getPOS()))
             return 0.0;             // if the pos tags are matched, the cost is 0
+        else
+            return 1.0;
+    }
+
+    /**
+     *
+     */
+    private static double doWS4JMatch(DNode dnode_T, DNode dnode_H) {
+        double[] ws4jSimilarities = computeWS4JSimilarity(dnode_T, dnode_H);
+        double average = StatUtils.sum(ws4jSimilarities) / (ws4jSimilarities.length * 1.0);
+        if (Double.compare(average, 0.5) > 0)
+            return 0.0;
         else
             return 1.0;
     }
@@ -88,18 +129,38 @@ public class VertexCost implements NodeComparer {
      * @param feaVector Feature vector
      * @return
      */
-    private static double computeExpFun(double[] weightVector, double[] feaVector) {
+    private static double computeExpFun(Vector<Double> weightVector, Vector<Double> feaVector) {
 
-        if (weightVector.length != feaVector.length) {
+        if (weightVector.size() != feaVector.size()) {
             System.err.println("Feature dimension and weight dimenstion are NOT identical!");
+            System.err.println("Feature = " + feaVector);
+            System.err.println("FeatureWeight = " + weightVector);
             System.exit(-1);
         }
 
         double tmp = 0.0;
-        for (int i = 0; i < feaVector.length; i++) {
-            tmp += (feaVector[i] * weightVector[i]);
+        for (int i = 0; i < feaVector.size(); i++) {
+            tmp += (feaVector.get(i) * weightVector.get(i));
         }
 
         return Math.exp(tmp) / (1 + Math.exp(tmp));
+    }
+
+    private static double[] computeWS4JSimilarity(DNode dNode_T, DNode dNode_H) {
+
+        double[] ws4jSimilarities = new double[rcs.length];
+        WS4JConfiguration.getInstance().setMFS(true);
+
+        int index = 0;
+        for ( RelatednessCalculator rc : rcs ) {
+            double s = rc.calcRelatednessOfWords(dNode_T.getForm(), dNode_H.getForm());
+            ws4jSimilarities[index++] = s;
+        }
+
+        System.out.println(dNode_T.getForm() + "\t" + dNode_H.getForm() + "\t");
+        for (double v : ws4jSimilarities)
+            System.out.print(v + ", ");
+        System.out.println();
+        return ws4jSimilarities;
     }
 }
